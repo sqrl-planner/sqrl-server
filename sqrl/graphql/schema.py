4ef15be6b9705bf9ee811bed617d097309aff1cf
galacticglum <nerftactic09@gmail.com>
2021-08-08 02:45:50 -0400

    Pagination for organisations and courses graphql endpoints

--- sqrl/graphql/schema.py
+++ sqrl/graphql/schema.py
@@ -1,4 +1,5 @@
-from typing import Any
+from typing import Any, Optional
+
 import graphene
 from graphene_mongo import MongoengineObjectType
 
@@ -42,22 +43,45 @@ class _OrganisationObject(MongoengineObjectType):
         model = Organisation
 
 
+class _OrganisationObjectConnection(graphene.relay.Connection):
+    """A connection for the Organisation object."""
+    class Meta:
+        node = _OrganisationObject
+
+
 class _CourseObject(MongoengineObjectType):
     """A course in the graphql schema."""
     class Meta:
         model = Course
 
 
+class _CourseObjectConnection(graphene.relay.Connection):
+    """A connection for the Course object."""
+    class Meta:
+        node = _CourseObject
+
+
 class Query(graphene.ObjectType):
     """A query in the graphql schema."""
-    organisations = graphene.List(_OrganisationObject)
-    courses = graphene.List(_CourseObject)
+    organisation = graphene.Field(_OrganisationObject, code=graphene.String())
+    organisations = graphene.ConnectionField(_OrganisationObjectConnection)
 
-    def resolve_organisations(self, info: Any) -> Any:
+    course = graphene.Field(_CourseObject, id=graphene.String())
+    courses = graphene.ConnectionField(_CourseObjectConnection)
+
+    def resolve_organisation(self, info: Any, code: str) -> Organisation:
+        """Return an _OrganisationObject object with the given code."""
+        return Organisation.objects.get(code=code)
+
+    def resolve_organisations(self, info: Any, **kwargs: Any) -> list[Organisation]:
         """Return a list of _OrganisationObject objects."""
         return list(Organisation.objects.all())
 
-    def resolve_courses(self, info: Any) -> Any:
+    def resolve_course(self, info: Any, id: str) -> Course:
+        """Return a _CourseObject object with the given id."""
+        return Course.objects.get(id=id)
+
+    def resolve_courses(self, info: Any, **kwargs: Any) -> list[Course]:
         """Return a list of _CourseObject objects."""
         return list(Course.objects.all())
 
