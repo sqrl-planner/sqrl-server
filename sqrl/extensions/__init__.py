0b232a92f867124876e5c644684352c0f7534f6d
galacticglum <nerftactic09@gmail.com>
2021-08-08 21:12:40 -0400

    CORS support

--- sqrl/extensions/__init__.py
+++ sqrl/extensions/__init__.py
@@ -2,9 +2,10 @@
 from flask import Flask
 
 from sqrl.extensions.db import db, init_app as _init_db
-
+from sqrl.extensions.cors import init_app as _init_cors
 
 def init_app(app: Flask) -> None:
     """Initialise extensions with a flask app context."""
     _init_db(app)
+    _init_cors(app)
     
