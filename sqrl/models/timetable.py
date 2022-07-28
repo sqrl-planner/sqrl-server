1f8677923dc05f615aded6bb3f1fbd31fd273f1c
galacticglum <nerftactic09@gmail.com>
2022-07-28 15:58:36 -0400

    timetable deletion

--- sqrl/models/timetable.py
+++ sqrl/models/timetable.py
@@ -235,11 +235,14 @@ class Timetable(db.Document):
     """A class representing a timetable.
 
     Instance Attributes:
+        name: The name of this timetable.
         key: The auth key for this timetable.
-        meetings: A mapping of course codes to list of section meetings selected for that course.
+        sections: A mapping of course codes to a list of sections for that course.
+        deleted: Whether this timetable has been deleted.
     """
 
     name: str = db.StringField(default=_generate_timetable_name)
     key: str = db.StringField(required=True, default=secrets.token_urlsafe)
     sections: dict[str, list[str]] = db.MapField(
         db.ListField(db.StringField()), default=dict)
+    deleted: bool = db.BooleanField(default=False)
