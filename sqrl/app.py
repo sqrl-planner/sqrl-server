a0106641d5247a8dfe5340a52c399fbc523a3492
GalacticGlum <nerftactic09@gmail.com>
2023-02-06 16:08:25 -0500

    fix: more pydocstyle errors

--- sqrl/app.py
+++ sqrl/app.py
@@ -7,12 +7,13 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 def create_app(settings_override: Any = None) -> Flask:
-    """
-    Create a Flask application using the app factory pattern. Return the app
-    instance.
+    """Create a Flask application using the app factory pattern.
 
     Args:
         settings_override: Override settings
+
+    Returns:
+        A Flask application instance.
     """
     app = Flask(__name__, static_folder='../public', static_url_path="")
     app.config.from_object('config.app_settings')
@@ -31,13 +32,12 @@ def create_app(settings_override: Any = None) -> Flask:
 
 
 def init_middleware(app: Flask) -> None:
-    """
-    Register 0 or more middleware (mutates the app passed in).
+    """Register 0 or more middleware (mutates the app passed in).
 
     Args:
         app: A Flask application instance.
     """
-    # Enable the Flask interactive debugger in the brower for development.
+    # Enable the Flask interactive debugger in the browser for development.
     if app.debug:
         app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
 
